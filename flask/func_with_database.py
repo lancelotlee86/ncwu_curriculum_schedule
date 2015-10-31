53ce26f580ebea168c6906edc16f017636a7716c
lancelotlee86 <lancelotlee86@gmail.com>
2015-10-31 10:22:03 +0800

    little fix
    删除一些注释

--- flask/func_with_database.py
+++ flask/func_with_database.py
@@ -8,29 +8,7 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-#try:
-    #with connection.cursor() as cursor:
-        # Create a new record
-        #sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        #cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
 
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-    #connection.commit()
-
-#    with connection.cursor() as cursor:
-#        sql = "SELECT `id`, `position` FROM `classroom`"
-#        cursor.execute(sql)
-#        results = cursor.fetchall()
-#        for result in results:
-#            print(result)
-        # Read a single record
-        #sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-        #cursor.execute(sql, ('webmaster@python.org',))
-        #result = cursor.fetchone()
-        #print(result)
-#finally:
-#    connection.close()
 
 def func_getPositionByClassroomId(classroom_id):
     ''' 通过给定的 classroom id 返回与之相应的 position
